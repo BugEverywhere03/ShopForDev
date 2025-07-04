c6e16197f7d2fb4a2aa7042ef9278f2c86c851de
Nguyễn Duy Dương <duongdevpython@gmail.com>
2025-07-04 15:07:49 +0700

    Init Project

---
+++ server.js
@@ -0,0 +1,11 @@
+import app from './src/app.js';
+
+const server = app.listen(3055, () => {
+    console.log('Server is running on port 3055');
+});
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Server closed');
+    });
+});
---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
+
+
+const app = express();
+
+// init middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+
+app.get('/', (req, res, next)  => {
+    return res.status(200).json({
+        message: "Welcome to ShopDev",
+    })
+})
+
+export default app;
